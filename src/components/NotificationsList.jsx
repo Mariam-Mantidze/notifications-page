0427030ede1fef47d6820c49be7129e286c7daee
Mariam <mariam.mantidze1@gmail.com>
2024-03-12 11:38:06 +0400

    style: message section

--- src/components/NotificationsList.jsx
+++ src/components/NotificationsList.jsx
@@ -8,16 +8,26 @@ export default function NotificationsList() {
         return (
           <Notifications key={message.id}>
             <img src={message.profilePic} alt="profile picture of a user" />
+            <div className="flex-container">
+              <div className="text-content">
+                <a className="sender-name" href="#">
+                  {message.senderName}
+                </a>
+                <p className="action">{message.action}</p>
+                <span className="post">{message.post}</span>
+                <a className="group-name" href="#">
+                  {message.groupName}
+                </a>
 
-            <div className="text-content">
-              <h3>{message.senderName}</h3>
-              <p className="action">{message.action}</p>
-              <span className="post">{message.post}</span>
-              <a className="group-name">{message.groupName}</a>
-              <p className="text">{message.text}</p>
-              {/* <img src={message.userPicture} alt="" /> */}
+                <p className="message-text">{message.text}</p>
+                <img
+                  className="user-picture"
+                  src={message.userPicture}
+                  alt=""
+                />
+              </div>
+              <span className="time">{message.time}</span>
             </div>
-            <span className="time">{message.time}</span>
           </Notifications>
         );
       })}
@@ -29,14 +39,19 @@ const NotificationContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 10px;
+  margin-top: 24px;
 `;
 
 const Notifications = styled.div`
   display: flex;
   gap: 13px;
-  align-items: center;
+  align-items: flex-start;
   padding: 16px;
   background-color: rgba(247, 250, 253, 1);
+
+  & a {
+    text-decoration: none;
+  }
 
   & img {
     width: 39px;
@@ -46,9 +61,10 @@ const Notifications = styled.div`
   & .text-content {
     display: flex;
     gap: 6px;
+    flex-wrap: wrap;
   }
 
-  & h3 {
+  & .sender-name {
     font-size: 14px;
     font-weight: 800;
     line-height: 18px;
@@ -74,5 +90,30 @@ const Notifications = styled.div`
     font-weight: 700;
     line-height: 18px;
     color: rgba(10, 50, 123, 1);
+  }
+
+  & .time {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 18px;
+    color: rgba(147, 156, 173, 1);
+  }
+
+  & .flex-container {
+    display: flex;
+    flex-direction: column;
+    gap: 3px;
+  }
+
+  & .message-text {
+    font-size: 14px;
+    font-weight: 500;
+    line-height: 18px;
+    color: rgba(94, 103, 120, 1);
+    background-color: rgba(255, 255, 255, 1);
+    padding: 16px;
+    border: 1px solid rgba(221, 231, 238, 1);
+    border-radius: 5px;
+    margin-top: 12px;
   }
 `;
